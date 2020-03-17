b5c2007ac77df209495071d1db255e11e402a4e2
Anton Lazarev <antonok35@gmail.com>
2020-03-17 18:53:37 -0400

    add basic keyboard descriptor

--- src/descriptor.rs
+++ src/descriptor.rs
@@ -45,3 +45,26 @@ pub struct MouseReport {
     pub x: i8,
     pub y: i8,
 }
+
+/// KeyboardReport describes a report and its companion descriptor that can be
+/// used to send keyboard button presses to a host and receive the status of the
+/// keyboard LEDs.
+#[gen_hid_descriptor(
+    (collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = KEYBOARD) = {
+        (usage_page = KEYBOARD, usage_min = 0xE0, usage_max = 0xE7) = {
+            #[packed_bits 8] #[item_settings data,variable,absolute] modifier=input;
+        };
+        (usage_page = LEDS, usage_min = 0x01, usage_max = 0x05) = {
+            #[packed_bits 5] #[item_settings data,variable,absolute] leds=output;
+        };
+        (usage_page = KEYBOARD, usage_min = 0x00, usage_max = 0x65) = {
+            #[item_settings data,array,absolute] keycodes=input;
+        };
+    }
+)]
+#[allow(dead_code)]
+pub struct KeyboardReport {
+    pub modifier: u8,
+    pub leds: u8,
+    pub keycodes: [u8; 6],
+}
