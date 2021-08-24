b8207330e95941a9c3e7eccfa08fa2c63d2d12ac
Jacob Alexander <haata@kiibohd.com>
2021-08-24 10:02:08 -0700

    Fix Keyboard HID descriptor to be compatible with boot mode hosts (#27)
    - Missing the reserved byte
    - BIOSs and other boot-mode hosts do not parse the hid descriptor so you
      must accept the exact packet format which includes a reserved byte
      between the modifier byte and 6 keys
    - Keyboard HID codes go up to 0xDD (Keypad Hexidecimal)
      * Technically it's safe to set the max to 0xFF in case non-standard hid
        codes are used.

--- src/descriptor.rs
+++ src/descriptor.rs
@@ -64,10 +64,13 @@ pub struct MouseReport {
         (usage_page = KEYBOARD, usage_min = 0xE0, usage_max = 0xE7) = {
             #[packed_bits 8] #[item_settings data,variable,absolute] modifier=input;
         };
+        (usage_min = 0x00, usage_max = 0xFF) = {
+            #[item_settings constant,variable,absolute] reserved=input;
+        };
         (usage_page = LEDS, usage_min = 0x01, usage_max = 0x05) = {
             #[packed_bits 5] #[item_settings data,variable,absolute] leds=output;
         };
-        (usage_page = KEYBOARD, usage_min = 0x00, usage_max = 0x65) = {
+        (usage_page = KEYBOARD, usage_min = 0x00, usage_max = 0xDD) = {
             #[item_settings data,array,absolute] keycodes=input;
         };
     }
@@ -75,6 +78,7 @@ pub struct MouseReport {
 #[allow(dead_code)]
 pub struct KeyboardReport {
     pub modifier: u8,
+    pub reserved: u8,
     pub leds: u8,
     pub keycodes: [u8; 6],
 }
